f841607845bfcc1bee809c9c691ccef754d20c90
Ethan <ethansimpson285@gmail.com>
2023-06-12 18:14:04 +0100

    build directory

---
+++ build/CMakeFiles/c1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c1.dir/chess_base.cpp.o"
+  "CMakeFiles/c1.dir/chess_base.cpp.o.d"
+  "c1"
+  "c1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/c1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
